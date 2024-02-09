8b27bfe23813f97712f6523934cb40c4ad385943
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-08 18:22:56 -0700

    Add animations to about page

--- src/components/aboutGrid/AboutImgGrid.tsx
+++ src/components/aboutGrid/AboutImgGrid.tsx
@@ -1,16 +1,49 @@
-import { ReactElement, FC } from "react";
+import { ReactElement, FC, useRef, useLayoutEffect } from "react";
 import "./AboutImgGrid.scss";
-import grid1 from "../../assets/images/cfa-grid-1.jpg"; 
-import grid2 from "../../assets/images/cfa-grid-2.jpg"; 
+import grid1 from "../../assets/images/cfa-grid-1.jpg";
+import grid2 from "../../assets/images/cfa-grid-2.jpg";
+import gsap from "gsap";
 
 const AboutImgGrid: FC = (): ReactElement => {
+  const large = useRef(null);
+  const small = useRef(null);
+  const comp = useRef(null);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.from(large.current, {
+        duration:0.8, 
+        y: -20,
+        x: 20, 
+        opacity: 0.2,
+        ease:"power1.in"
+      })
+      gsap.from(small.current, {
+        duration:0.8, 
+        y: 20,
+        x: -20, 
+        opacity: 0.2,
+        ease:"power1.in"
+      })
+    }, comp);
+
+    return () => ctx.revert(); 
+  }, [])
   return (
-    <div className="about-img-grid">
-          <div className="about-img-grid__div--large">
-              <img src={ grid2 } alt="A young kid writing with a pencil" className="about-img-grid__img"/>
+    <div className="about-img-grid" ref={ comp }>
+      <div className="about-img-grid__div--large" ref={large}>
+        <img
+          src={grid2}
+          alt="A young kid writing with a pencil"
+          className="about-img-grid__img"
+        />
       </div>
-          <div className="about-img-grid__div--small">
-              <img src={ grid1 } alt="A young kid with headphones on" className="about-img-grid__img"/>
+      <div className="about-img-grid__div--small" ref={small}>
+        <img
+          src={grid1}
+          alt="A young kid with headphones on"
+          className="about-img-grid__img"
+        />
       </div>
     </div>
   );
--- src/components/aboutValues/AboutValues.tsx
+++ src/components/aboutValues/AboutValues.tsx
@@ -1,14 +1,50 @@
-import { ReactElement, FC } from "react";
+import { ReactElement, FC, useRef, useLayoutEffect } from "react";
 import "./AboutValues.scss";
 import grid4 from "../../assets/images/cfa-grid-4.jpg";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const AboutValues: FC = (): ReactElement => {
+    const comp = useRef(null); 
+    const img = useRef(null);
+
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+            gsap.from(img.current, {
+                duration: 0.8, 
+                y: 30,
+                x: 20,
+                opacity: 0, 
+                scrollTrigger: img.current,
+                ease:"power1.inOut"
+         })
+         gsap.utils.toArray<HTMLElement>(".values__item").forEach((item) => {
+            gsap.from(item, {
+              opacity: 0,
+                y: 50,
+                x: 20,
+              scrollTrigger: {
+                trigger: item,
+                start: "top 80%",
+                toggleActions: "play none none none",
+              },
+              stagger: {
+                amount: 0.5, // Stagger amount
+              },
+            });
+          });
+        }, comp);
+        
+        return () => ctx.revert();
+  }, [])
+
   return (
-    <section className="values">
+    <section className="values" ref={comp}>
       <div className="values__container">
         <div className="values__top">
           <div className="values__top--primary"></div>
-          <div className="values__top--main">
+          <div className="values__top--main" ref={img}>
             <img
               src={grid4}
               alt="Boy sitting on a chair and smiliing"

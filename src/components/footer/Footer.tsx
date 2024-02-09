8e46484c8ae5ba9a4fd3b16b5df0cf9b4fa95575
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-08 18:32:45 -0700

    Add link to email address

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -19,7 +19,7 @@ const Footer: FC = (): ReactElement => {
           <div>
             <h4 className="footer__header">Contact</h4>
             <p className="footer__p">Phone: (519) 697-3683 </p>
-              <p className="footer__p">Email: abasstijani@careforall.ca</p>
+              <a className="footer__p" href={`mailto:abasstijani@careforall.ca`}>Email: abasstijani@careforall.ca</a>
               <div>
                 <a href="https://www.instagram.com/careforall.ca/" target="blank" className="footer__icon-link">
                   <FaInstagram className="footer__icon"/>

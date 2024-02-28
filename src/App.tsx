d0c18517feed0b0e659b2146042218555dd5b178
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-28 00:30:37 -0700

    Add waitlist form and style waitlist component.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import AboutPage from './pages/about/AboutPage';
 import ServicesPage from './pages/services/ServicesPage';
 import EventsPage from './pages/events/EventsPage';
 import CareersPage from './pages/careers/CareersPage';
+import WaitList from './pages/waitlist/WaitList';
 
 
 const App:FC = ():ReactElement => {
@@ -22,6 +23,7 @@ const App:FC = ():ReactElement => {
           <Route path='/services' element={<ServicesPage/> } />
           <Route path='/events' element={<EventsPage/> } />
           <Route path='/careers' element={<CareersPage/> } />
+          <Route path='/waitlist' element={<WaitList/> } />
         </Routes>
         <Footer/>
       </BrowserRouter>
---
+++ src/pages/waitlist/WaitList.tsx
@@ -0,0 +1,40 @@
+import "./WaitList.scss";
+import { Link } from "react-router-dom";
+const WaitList = () => {
+  return (
+    <section>
+      <div className="waitlist">
+        <div className="waitlist__container">
+          <div className="waitlist__div">
+            <div className="waitlist__heading-div">
+              <h1 className="waitlist__heading">Waitlist</h1>
+              <p>Sign up for the chance to get a free month's consultation. </p>
+            </div>
+            <div className="waitlist__form-div">
+              <form className="waitlist__form">
+                <input
+                  type="text"
+                  placeholder="Enter your full name"
+                  className="waitlist__input"
+                />
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  className="waitlist__input"
+                />
+                <input
+                  type="submit"
+                  value="Join the waitlist"
+                  className="waitlist__btn"
+                />
+              </form>
+              <Link to={"/"} className="waitlist__link">Go home</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WaitList;

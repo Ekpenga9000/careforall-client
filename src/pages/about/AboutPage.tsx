6ff1195666dc853e895477da0415ddbcc4dc047d
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-27 17:31:12 -0700

    Add Ceo component to page.

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,23 +1,25 @@
-import { ReactElement, FC } from 'react'; 
-import "./AboutPage.scss"; 
-import AboutHero from '../../components/aboutHero/AboutHero';
-import AboutVision from '../../components/aboutVision/AboutVision';
-import AboutValues from '../../components/aboutValues/AboutValues';
-import CTA from '../../components/cta/CTA';
-import Contact from '../../components/contact-form/Contact';
+import { ReactElement, FC } from "react";
+import "./AboutPage.scss";
+import AboutHero from "../../components/aboutHero/AboutHero";
+import AboutVision from "../../components/aboutVision/AboutVision";
+import AboutValues from "../../components/aboutValues/AboutValues";
+import CTA from "../../components/cta/CTA";
+import Contact from "../../components/contact-form/Contact";
+import MeetCeo from "../../components/meetCeo/MeetCeo";
 
-const AboutPage: FC = ():ReactElement => {
+const AboutPage: FC = (): ReactElement => {
   return (
-      <section className='about'>
-          <div className="about__container">
-              <AboutHero />
-              <AboutVision />
-              <AboutValues />
-              <CTA msg=' Compassion guides our actions as we prioritize inclusivity, respect, and integrity. We are committed to collaboration, embracing diversity, and advocating for the rights and health equity of individuals with disabilities.' />
-              <Contact/>
-        </div>
+    <section className="about">
+      <div className="about__container">
+        <AboutHero />
+        <AboutVision />
+        <AboutValues />
+        <MeetCeo />
+        <CTA msg=" Compassion guides our actions as we prioritize inclusivity, respect, and integrity. We are committed to collaboration, embracing diversity, and advocating for the rights and health equity of individuals with disabilities." />
+        <Contact />
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default AboutPage;

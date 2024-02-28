a24f5f845c1c061628594f30073bf0586fe5e685
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-28 00:10:43 -0700

    Add content to events component.

--- src/pages/events/EventsPage.tsx
+++ src/pages/events/EventsPage.tsx
@@ -1,9 +1,23 @@
+import Contact from "../../components/contact-form/Contact";
+import "./EventsPage.scss";
+
 const EventsPage = () => {
   return (
-    <section>
-      <h1>Events Page</h1>
+    <section className="events">
+      <div className="events__container">
+        <div className="events__div">
+                  <h1 className="events__heading">Events</h1>
+                  
+                    <h2>Special Announcement!</h2>
+          <p>
+            We are pleased to announce that we are officially launching on March
+            1st, 2024!{" "}
+          </p>
+              </div>
+              <Contact />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default EventsPage
+export default EventsPage;
